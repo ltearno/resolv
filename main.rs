66c822680d0d50e95ef2b0a9a5aff97e33adc67c
Arnaud Tournier <arnaud.tournier@stack-labs.com>
2019-02-20 15:26:51 +0100

    wip error management

--- main.rs
+++ main.rs
@@ -1,9 +1,14 @@
+extern crate getopts;
+use getopts::Options;
+use std::env;
+
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufRead;
 use std::io::BufReader;
 use std::io::BufWriter;
+use std::result::Result;
 use std::time::{SystemTime, UNIX_EPOCH};
 
 #[derive(Debug)]
@@ -59,13 +64,15 @@ enum State {
     Completing,
 }
 
-fn fetch_lines<'a>() -> Vec<String> {
-    let f = File::open("Resolvfile").expect("did not find Resolvefile");
-    let buf_reader = BufReader::new(&f);
-    buf_reader
-        .lines()
-        .map(|line| line.expect("cannot read line"))
-        .collect()
+fn fetch_lines(path: &str) -> Result<Vec<String>, String> {
+    match File::open(path) {
+        Ok(f) => Ok(BufReader::new(&f)
+            .lines()
+            .map(|line| line.expect("cannot read line"))
+            .collect()),
+
+        Err(e) => Result::Err(format!("did not find {}", path)),
+    }
 }
 
 fn complete_rule<'a>(r: &mut Rule<'a>, line: &'a str) {
@@ -139,6 +146,21 @@ fn store_state_store(rules_state_store: &HashMap<&str, u64>) {
 fn main() {
     println!("Resolv v0.1, welcome\n");
 
+    let args: Vec<String> = env::args().collect();
+    let mut opts = Options::new();
+    opts.optopt("f", "file", "set input file name", "NAME");
+    //opts.optflag("h", "help", "print this help menu");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(f) => panic!(f.to_string()),
+    };
+
+    let resolve_file_path = match matches.opt_str("f") {
+        Some(path) => String::from(path),
+        None => String::from("Resolvfile"),
+    };
+
     let mut rules_state_store: HashMap<&str, u64> = HashMap::new();
 
     let state_store_from_settings = read_state_store();
@@ -146,18 +168,23 @@ fn main() {
         rules_state_store.insert(name, *value);
     }
 
-    let lines = fetch_file();
-    let rules = parse_rules(&lines);
+    let lines = fetch_file(&resolve_file_path);
+    match lines {
+        Ok(lines) => {
+            let rules = parse_rules(&lines);
 
-    let to_build: &str = match rules[0].name {
-        None => "",
-        Some(name) => name,
-    };
+            let to_build: &str = match rules[0].name {
+                None => "",
+                Some(name) => name,
+            };
 
-    println!("run for target '{}'", to_build);
-    run_for_rule(&rules, to_build, &mut rules_state_store);
+            println!("run for target '{}'", to_build);
+            run_for_rule(&rules, to_build, &mut rules_state_store);
 
-    store_state_store(&rules_state_store);
+            store_state_store(&rules_state_store);
+        }
+        Err(e) => println!("[ERROR] {}", e),
+    }
 
     println!("done");
 }
@@ -261,11 +288,15 @@ fn find_rule<'a, 'b>(rules: &'a Vec<Rule<'a>>, search: &'b str) -> Option<&'a Ru
     None
 }
 
-fn fetch_file() -> Vec<String> {
-    let mut lines = fetch_lines();
-    lines.push(String::from(""));
+fn fetch_file(path: &str) -> Result<Vec<String>, String> {
+    let lines = fetch_lines(path);
+    if lines.is_err() {
+        return lines;
+    }
 
-    lines
+    let mut lines = lines.unwrap();
+    lines.push(String::from(""));
+    Ok(lines)
 }
 
 fn push_and_prepare<'a>(rules: &mut Vec<Rule<'a>>, rule: Option<Rule<'a>>) -> Rule<'a> {
